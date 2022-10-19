201221ab62ff3f7022985f144d78d88aca34a26c
Peter <peterpeev28@gmail.com>
2022-10-19 21:37:27 +0300

    02-3

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// basics - output and comments # 1
+// --------- basics - output and comments # 1 ---------
 // console.log("Hello World");
 // console.log("Do you like pizza?");
 // console.log("It's really good.");
 
 // window.alert("I really love it! :D");
 
-//variables # 2
-let firstName = "Peter"; // string
-let age = 21;            // int
-let student = true;     // boolean 
+// --------- variables # 2 ---------
+// let firstName = "Peter"; // string
+// let age = 21;            // int
+// let student = true;     // boolean 
 
-//Output into the console
-console.log("Hello", firstName);
-console.log("You are ", age, "Years old.");
-console.log("Enroled:", student);
+// //Output into the console
+// console.log("Hello", firstName);
+// console.log("You are ", age, "Years old.");
+// console.log("Enroled:", student);
 
-//Output into the web-site thro elements in HTML
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + "Years old";
-document.getElementById("p3").innerHTML = "Enroled: " + student;
+// //Output into the web-site thro elements in HTML
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + "Years old";
+// document.getElementById("p3").innerHTML = "Enroled: " + student;
 
-
+// --------- arithmetic operations # 3 ---------
 
 
