18caca5708ff2db1397a7ab9185324a007f438a0
Peter <peterpeev28@gmail.com>
2022-10-19 21:31:56 +0300

    02

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-// Comment 
+// basics - output and comments # 1
+// console.log("Hello World");
+// console.log("Do you like pizza?");
+// console.log("It's really good.");
 
-console.log("Hello World");
-console.log("Do you like pizza?");
-console.log("It's really good.");
+// window.alert("I really love it! :D");
 
-window.alert("I really love it! :D");
+//variables # 2
+let firstName = "Peter"; // string
+let age = 21;            // int
+let student = true;     // boolean 
+
+
+
+console.log("Hello", firstName);
+console.log("You are ", age, "Years old.");
+console.log("Enroled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + "Years old";
+document.getElementById("p3").innerHTML = "Enroled: " + student;
+
+
+
+
