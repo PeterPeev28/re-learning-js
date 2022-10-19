dac954c915778db306d6bf4ccb2c7f63335b5a85
Peter <peterpeev28@gmail.com>
2022-10-19 21:34:02 +0300

    02-2

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ let firstName = "Peter"; // string
 let age = 21;            // int
 let student = true;     // boolean 
 
-
-
+//Output into the console
 console.log("Hello", firstName);
 console.log("You are ", age, "Years old.");
 console.log("Enroled:", student);
 
+//Output into the web-site thro elements in HTML
 document.getElementById("p1").innerHTML = "Hello " + firstName;
 document.getElementById("p2").innerHTML = "You are " + age + "Years old";
 document.getElementById("p3").innerHTML = "Enroled: " + student;
