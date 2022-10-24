258654d4a9720296e2ac12612792a8b21475141d
Peter Peev <peterpeev28@gmail.com>
2022-10-24 11:48:05 +0300

    monday commit

---
+++ ilearned.js
@@ -0,0 +1,16 @@
+/*
+In this file i will writedown my notes thro my js learning experience
+
+1. Basics
+console logging - console.jog
+
+2. Javascript primitive types
+- Null
+- Undefined
+- NaN - Not a Number
+- Number
+- Boolean
+- BigInt
+- String
+- Symbol (char)
+*/
